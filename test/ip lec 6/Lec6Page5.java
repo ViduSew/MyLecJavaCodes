b34c5582f7210a7843f6cf32aa70f0c6e8be6dc2
Vidushi Sewmika <116452885+ViduSew@users.noreply.github.com>
2025-02-26 04:58:23 +0530

    Add files via upload

---
+++ test/ip lec 6/Lec6Page5.java
@@ -0,0 +1,12 @@
+//Displaythenumbers1,2,3,4,5 using do-while
+public class Lec6Page5{
+
+	public static void main(String[] args){
+ 
+            int number=1;
+            do {
+                System.out.println(number);
+                ++number;
+            } while(number<=5);
+        }		
+}
