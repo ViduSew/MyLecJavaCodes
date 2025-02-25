b3a9ad19686df00640ac463dffc41033b3ad174e
Vidushi Sewmika <116452885+ViduSew@users.noreply.github.com>
2025-02-26 04:56:33 +0530

    Add files via upload

---
+++ test/ip lec 5/Lec5Page5.java
@@ -0,0 +1,12 @@
+//Display 1,2,3,4,5 with while statement
+public class Lec5Page5{
+
+	public static void main(String[] args){
+            int number=1;
+            while(number<=5){
+                System.out.println(number);
+                ++number;
+            }
+            
+        }		
+}
